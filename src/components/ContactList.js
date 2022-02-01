16ec36985bcedb476c3e0ce845f993026064ec68
thoriqnoer <thoriq.noer@gmail.com>
2022-02-01 15:08:00 +0700

    call context and create usersearch function

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -4,16 +4,23 @@ import { useContactsCrud } from '../context/ContactsCrudContext';
 import ContactCard from './ContactCard';
 
 const ContactList = () => {
-  const { contacts, getAllContacts } = useContactsCrud();
+  const { contacts, getAllContacts, searchHandler, text, searchResults } =
+    useContactsCrud();
 
   useEffect(() => {
     getAllContacts();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const renderContactList = contacts.map((contact) => {
-    return <ContactCard contact={contact} key={contact.id} />;
-  });
+  const renderContactList = (text.length < 1 ? contacts : searchResults).map(
+    (contact) => {
+      return <ContactCard contact={contact} key={contact.id} />;
+    }
+  );
+
+  const onUserSearch = (e) => {
+    searchHandler(e.target.value);
+  };
 
   return (
     <div className="ui main">
@@ -23,6 +30,18 @@ const ContactList = () => {
           <button className="ui right floated button blue">Add Contact</button>
         </Link>
       </h2>
+      <div className="ui search">
+        <div className="ui icon input">
+          <input
+            type="text"
+            placeholder="Search Contacts"
+            className="prompt"
+            value={text}
+            onChange={(e) => onUserSearch(e)}
+          />
+          <i className="search icon"></i>
+        </div>
+      </div>
       <div
         className="ui very relaxed list "
         style={{ width: '30em', marginTop: '2rem' }}
