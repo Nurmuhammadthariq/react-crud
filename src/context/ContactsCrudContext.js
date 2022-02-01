f2fbd6ed2f0f362ae570d78ba8cf185216462f51
thoriqnoer <thoriq.noer@gmail.com>
2022-02-01 15:09:09 +0700

    create state hook text input and search results

--- src/context/ContactsCrudContext.js
+++ src/context/ContactsCrudContext.js
@@ -6,6 +6,8 @@ const contactsCrudContext = createContext();
 
 export function ContactsCrudContextProvider({ children }) {
   const [contacts, setContacts] = useState([]);
+  const [text, setText] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   // Get all contacts
   const getAllContacts = async () => {
@@ -32,11 +34,40 @@ export function ContactsCrudContextProvider({ children }) {
     setContacts(newContact);
   };
 
+  const updateContactHandler = async (contact) => {
+    const response = await api.put(`/contacts/${contact.id}`, contact);
+    const { id } = response.data;
+    setContacts(
+      contacts.map((contact) => {
+        return contact.id === id ? { ...response.data } : contact;
+      })
+    );
+  };
+
+  const searchHandler = (searchTerm) => {
+    setText(searchTerm);
+    if (searchTerm !== '') {
+      const newContactList = contacts.filter((contact) => {
+        return Object.values(contact)
+          .join(' ')
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase());
+      });
+      setSearchResults(newContactList);
+    } else {
+      setSearchResults(contacts);
+    }
+  };
+
   const value = {
     contacts,
     getAllContacts,
     addContactHandler,
     removeContactHandler,
+    updateContactHandler,
+    searchHandler,
+    text,
+    searchResults,
   };
 
   return (
